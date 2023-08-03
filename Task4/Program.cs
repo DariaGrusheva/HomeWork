0885c65593902fc2687bd01d38820864a087f4b3
Daria <gdaria1609@yandex.ru>
2023-08-03 15:00:12 +0500

    add_check_Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,12 +2,33 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Введите натуральное число");
+Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
 int i = 2;
 
-while(i <= number)
+if (number == 1)
 {
-     Console.Write(i + ", ");
-    i = i + 2;
+    Console.Write("В промежутке до 1 нет четных натуральных чисел.");
+}
+else
+{
+    Console.Write("Промежуток (от 1 до введенного Вами числа) содержит следующие четные числа: ");
+    while (i <= number)
+    {
+        if (i < number - 1)
+        {
+            Console.Write(i + ", ");
+        }
+        else if (i == number - 1)
+        {
+            Console.Write(i + ".");
+        }
+        else
+        {
+            Console.Write(i + ".");
+        }
+
+        i = i + 2;
+    }
 }
